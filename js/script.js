0964c5c4b9c78699569a8693be53a2f52416e4a9
LilitHov <hlilit19@gmail.com>
2018-10-08 16:37:31 +0400

    global styles,headings and sign in

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// floating label
+var input = document.querySelectorAll('.form-input');
+// var label = document.querySelectorAll('.form-label');
+
+
+for (var i=0;i < input.length; i++) {
+    input[i].onblur = function () {
+        // if (this.classList.contains('form-input')) {
+        //     alert('true')
+        // }
+        var label = this.parentElement.querySelector('.form-label');
+        if (this.value !== "") {
+           label.classList.add('floating')
+       } else {
+           label.classList.remove('floating')
+       }
+    }
+}
