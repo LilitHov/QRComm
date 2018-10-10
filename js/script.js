1d5506ba3e645a800e26471410f44ff1f3387feb
LilitHov <hlilit19@gmail.com>
2018-10-10 14:57:57 +0400

    Sign in Page /responsive

--- js/script.js
+++ js/script.js
@@ -3,16 +3,37 @@ var input = document.querySelectorAll('.form-input');
 // var label = document.querySelectorAll('.form-label');
 
 
-for (var i=0;i < input.length; i++) {
+for (var i = 0; i < input.length; i++) {
     input[i].onblur = function () {
         // if (this.classList.contains('form-input')) {
         //     alert('true')
         // }
         var label = this.parentElement.querySelector('.form-label');
         if (this.value !== "") {
-           label.classList.add('floating')
-       } else {
-           label.classList.remove('floating')
-       }
+            label.classList.add('floating')
+        } else {
+            label.classList.remove('floating')
+        }
+    }
+}
+
+
+// Tooltip
+
+var tooltip = document.querySelectorAll('.tooltip');
+console.log(tooltip);
+var text = '';
+for (var i = 0; i < tooltip.length; i++) {
+    tooltip[i].onmouseover = function () {
+        text = this.dataset.text;
+        if (text !== undefined) {
+           var tooltipText = this.nextElementSibling;
+            tooltipText.style.opacity = 1;
+            tooltipText.innerText = text;
+        }
+    };
+
+    tooltip[i].onmouseout = function () {
+        this.nextElementSibling.style.opacity = 0;
     }
 }
