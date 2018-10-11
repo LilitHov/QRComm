0a2a9dac42c0a128107a21081c10c0b031e5fc2a
LilitHov <hlilit19@gmail.com>
2018-10-11 15:36:05 +0400

    list of locations page

--- js/script.js
+++ js/script.js
@@ -5,36 +5,11 @@ var input = document.querySelectorAll('.form-input');
 
 for (var i = 0; i < input.length; i++) {
     input[i].onblur = function () {
-        // if (this.classList.contains('form-input')) {
-        //     alert('true')
-        // }
         var label = this.parentElement.querySelector('.form-label');
         if (this.value !== "") {
             label.classList.add('floating')
         } else {
             label.classList.remove('floating')
-        }
-    }
-}
-
-
-// Tooltip
-
-var tooltips = document.querySelectorAll('.tooltip');
-// console.log(tooltips);
-var text = '';
-var tooltipsLength = tooltips.length;
-for (let i = 0; i < tooltipsLength; i++) {
-    var tooltipText = tooltips[i].nextElementSibling;
-    text = tooltips[i].dataset.text;
-    tooltipText.innerText = text;
-    if (text !== undefined) {
-        tooltips[i].onmouseover = function () {
-            tooltipText.classList.add('show');
-        };
-
-        tooltips[i].onmouseout = function () {
-            tooltipText.classList.remove('show');
         }
     }
 }
