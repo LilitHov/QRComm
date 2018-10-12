1cf459264197d74ee352047436160a0159861a70
LilitHov <hlilit19@gmail.com>
2018-10-12 19:05:42 +0400

    all pages

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ var input = document.querySelectorAll('.form-input');
 
 for (var i = 0; i < input.length; i++) {
     input[i].onblur = function () {
-        var label = this.parentElement.querySelector('.form-label');
+        let label = this.parentElement.querySelector('.label');
         if (this.value !== "") {
             label.classList.add('floating')
         } else {
@@ -13,3 +13,20 @@ for (var i = 0; i < input.length; i++) {
         }
     }
 }
+
+
+$(document).ready(function () {
+    // select2
+    $('.select2').select2({
+        minimumResultsForSearch: -1,
+        placeholder: false,
+    });
+
+    $('.select2').on('select2:selecting', function (e) {
+        let label = this.parentElement.querySelector('.label');
+        label.classList.add('floating');
+    });
+
+    // date picker
+    $('[data-toggle="datepicker"]').datepicker();
+});
