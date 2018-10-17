0c31fa730d683c32bd148cb430c249f820742757
LilitHov <hlilit19@gmail.com>
2018-10-17 17:57:27 +0400

    floating labels and tooltips

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-// floating label
-var input = document.querySelectorAll('.form-input');
-// var label = document.querySelectorAll('.form-label');
-
-
-for (var i = 0; i < input.length; i++) {
-    input[i].onblur = function () {
-        let label = this.parentElement.querySelector('.label');
-        if (this.value !== "") {
-            label.classList.add('floating')
-        } else {
-            label.classList.remove('floating')
-        }
-    }
-}
-
-
 $(document).ready(function () {
     // select2
     $('.select2').select2({
@@ -27,6 +10,71 @@ $(document).ready(function () {
         label.classList.add('floating');
     });
 
+    // Tooltip
+    var toolTitle = document.querySelectorAll('.tool-title');
+    var toolLength = toolTitle.length;
+    for (let i = 0; i < toolLength; i++) {
+        let titleWidth = toolTitle[i].offsetWidth;
+        let tooltipWidth = toolTitle[i].parentElement.offsetWidth;
+        let toolText = toolTitle[i].parentElement.nextElementSibling;
+        if (tooltipWidth < titleWidth) {
+            toolTitle[i].onmouseover = function () {
+                toolText.classList.add('show');
+            };
+
+            toolTitle[i].onmouseout = function () {
+                toolText.classList.remove('show');
+            };
+        }
+    }
+
     // date picker
-    $('[data-toggle="datepicker"]').datepicker();
+    $('[data-toggle="datepicker"]').datepicker({});
+
+    // floating label
+    var date = document.querySelectorAll('.form-input');
+    var dateLength = date.length;
+
+    for (var i = 0; i < dateLength; i++) {
+        // onfocus
+        date[i].onfocus = function () {
+            addFloating(this);
+        };
+        // onchange
+        date[i].onchange = function () {
+            if (this.value !== '') {
+                addFloating(this);
+            } else {
+                removeFloating(this);
+            }
+        };
+        // onblur
+        date[i].onblur = function () {
+            if(!this.classList.contains('date')) {
+                if (this.value !== '') {
+                    addFloating(this);
+                } else {
+                    removeFloating(this);
+                }
+            }
+        };
+    }
+    // onhide
+    $('[data-toggle="datepicker"]').on('hide.datepicker', function (e) {
+        if(this.value === "") {
+            removeFloating(this);
+        }
+    });
 });
+
+// add floating class
+function addFloating(e) {
+    let label = e.parentElement.querySelector('.label');
+    label.classList.add('floating');
+}
+
+//remove floating class
+function removeFloating(e) {
+    let label = e.parentElement.querySelector('.label');
+    label.classList.remove('floating');
+}
